090d472f63a84345b8fd71a003be1f28997eaab5
bishnubd26 <bishnunath820@gmail.com>
2024-10-13 07:42:58 +0600

    created utilities function for multiple calls

--- js/addMoney.js
+++ js/addMoney.js
@@ -1,4 +1,16 @@
+/**
+ * How to get a number from an input field
+ * 1. create a variable
+ * 2. right side document.getElementById(id)
+ * 3. .value
+ * 4. parseFloat
+*/
+
 document.getElementById('btn-add-money')
   .addEventListener('click', function (event) {
     event.preventDefault();
+
+    const addMoney = getInputFieldValueById('input-add-money');
+    const pinNumber = getInputFieldValueById('input-pin-number');
+    console.log('add money value with parameter: ', addMoney, pinNumber);
   })
--- js/utilities.js
+++ js/utilities.js
@@ -2,6 +2,13 @@
  * common shared functions here
 */
 
-function getInputFieldValueById() {
-    console.log('will get value by id');
+/* function getInputFieldValueById() {
+  const addMoney = document.getElementById('input-add-money').value;
+  return addMoney;
+} */
+
+
+function getInputFieldValueById(id) {
+  const inputValue = document.getElementById(id).value;
+  return inputValue;
 }
