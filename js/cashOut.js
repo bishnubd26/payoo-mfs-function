5cadfeee5bc17e92175475caa461a58d92692fbd
bishnubd26 <bishnunath820@gmail.com>
2024-10-14 07:15:58 +0600

    Dynamic DOM and data validation

--- js/cashOut.js
+++ js/cashOut.js
@@ -4,11 +4,33 @@ document.getElementById('btn-cash-out')
 
     const cashOut = getInputFieldValueById('input-cash-out');
     const pinNumber = getInputFieldValueById('input-cash-out-pin');
+
+    // validation
+    if (isNaN(cashOut)) {
+      alert('Failed to cash out.');
+      return;
+    }
     
     if (pinNumber === 1234) {
       const balance = getTextFieldValueById('account-balance');
+
+      if (cashOut > balance) {
+        alert('You do not have enough money to cash out.');
+        return;
+      }
+
       const newBalance = balance - cashOut;
       document.getElementById('account-balance').innerText = newBalance;
+
+      // add to transaction history
+      const div = document.createElement('div');
+      div.classList.add('bg-yellow-300');
+      div.innerHTML = `
+        <h2 class="text-2xl font-bold">Cash Out</h2>
+        <p>${cashOut} withdraw. New balance ${newBalance}</p>
+      `;
+
+      document.getElementById('transaction-container').appendChild(div);
     }
     else{
       alert('No money for you...');
